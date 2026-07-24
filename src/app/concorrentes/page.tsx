0da60551203425765b9c92fbd299e6486c73b4ef
martinsjorgeanef <martinsjorgeanef@gmail.com>
2026-07-24 13:01:30 -0300

    Wrap ManageCompetitorsScreen in AppShell component

--- src/app/concorrentes/page.tsx
+++ src/app/concorrentes/page.tsx
@@ -1,5 +1,10 @@
+import { AppShell } from "@/components/AppShell";
 import { ManageCompetitorsScreen } from "@/components/ManageCompetitorsScreen";
 
 export default function ConcorrentesPage() {
-  return <ManageCompetitorsScreen />;
+  return (
+    <AppShell>
+      <ManageCompetitorsScreen />
+    </AppShell>
+  );
 }
