987115789890447716cf8669c44dfd65c47e8024
martinsjorgeanef <martinsjorgeanef@gmail.com>
2026-07-24 12:55:19 -0300

    Create ComprasPage component with AppShell and ComprasScreen

---
+++ src/app/compras/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/AppShell";
+import { ComprasScreen } from "@/components/ComprasScreen";
+
+export default function ComprasPage() {
+  return (
+    <AppShell>
+      <ComprasScreen />
+    </AppShell>
+  );
+}
