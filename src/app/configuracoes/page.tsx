edb59aeadc12a3bde09dc46f68c561f2dfc3ecd4
martinsjorgeanef <martinsjorgeanef@gmail.com>
2026-07-24 12:58:25 -0300

    Create ConfiguracoesPage component with AppShell

---
+++ src/app/configuracoes/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/AppShell";
+import { ConfiguracoesScreen } from "@/components/ConfiguracoesScreen";
+
+export default function ConfiguracoesPage() {
+  return (
+    <AppShell>
+      <ConfiguracoesScreen />
+    </AppShell>
+  );
+}
