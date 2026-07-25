d02b7a9e55d6a088ac2f16e5538e683229c11f1b
martinsjorgeanef <martinsjorgeanef@gmail.com>
2026-07-25 12:57:20 -0300

    Add function to export compras data to Excel

---
+++ src/lib/comprasExcelExport.ts
@@ -0,0 +1,44 @@
+import * as XLSX from "xlsx";
+import { IndustryRow, CategoryRow } from "./types";
+import { buildComprasActionRecommendations } from "./comprasEmailBuilder";
+
+export function exportComprasExcel(industry: IndustryRow | undefined, categories: CategoryRow[]) {
+  var wb = XLSX.utils.book_new();
+
+  var resumoRows = [
+    ["Fornecedor", industry ? industry.fornecedor : "-"],
+    ["Itens Martins", industry ? industry.itensMartins : 0],
+    ["Itens Concorrente Cadastrados", industry ? industry.itensConcorrenteCadastrados : 0],
+    ["Diferenca de Mix", industry ? industry.diferenca : 0],
+    ["Posicionamento (%)", industry ? industry.competitivePct : 0],
+    ["Itens em Desvantagem", industry ? industry.disadvantage : 0],
+    ["Prioridade", industry ? industry.priority : "-"]
+  ];
+  var resumoSheet = XLSX.utils.aoa_to_sheet(resumoRows);
+  resumoSheet["!cols"] = [{ wch: 28 }, { wch: 30 }];
+  XLSX.utils.book_append_sheet(wb, resumoSheet, "Resumo Industria");
+
+  var catHeader = ["Categoria", "Monitorados", "Competitivos", "Em Desvantagem", "Competitividade (%)", "Gap Medio (%)", "Prioridade"];
+  var catRows = categories.map(function (c) {
+    return [
+      c.category,
+      c.monitored,
+      c.competitive,
+      c.disadvantage,
+      c.competitivePct,
+      c.avgDisadvantagePct !== null ? c.avgDisadvantagePct : "-",
+      c.priority
+    ];
+  });
+  var catSheet = XLSX.utils.aoa_to_sheet([catHeader].concat(catRows));
+  catSheet["!cols"] = [{ wch: 30 }, { wch: 14 }, { wch: 14 }, { wch: 16 }, { wch: 18 }, { wch: 14 }, { wch: 18 }];
+  XLSX.utils.book_append_sheet(wb, catSheet, "Categorias");
+
+  var acoes = buildComprasActionRecommendations(categories);
+  var acoesSheet = XLSX.utils.aoa_to_sheet([["Acoes Recomendadas"]].concat(acoes.map(function (a) { return [a]; })));
+  acoesSheet["!cols"] = [{ wch: 80 }];
+  XLSX.utils.book_append_sheet(wb, acoesSheet, "Acoes Recomendadas");
+
+  var fileName = "analise_compradores_" + (industry ? industry.fornecedor.replace(/\s+/g, "_") : "geral") + ".xlsx";
+  XLSX.writeFile(wb, fileName);
+}
